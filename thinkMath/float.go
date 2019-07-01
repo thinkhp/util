2fb026baa195fffbaf51c0b1491f006bd67670ee
禾沛 <13730645556@163.com>
2019-07-01 11:40:44 +0800

    mathUtil

--- thinkMath/float.go
+++ thinkMath/float.go
@@ -1,9 +1,22 @@
 package thinkMath
 
-import "math"
+import (
+	"math"
+	"strconv"
+)
 
 func Round(f float64, i int) float64 {
 	//f += math.Pow10(-15)
 	n := math.Pow10(i)
 	return math.Trunc((f+0.5/n)*n) / n
+}
+
+func KeepDecimal(f float64, i int) float64 {
+	m := math.Pow10(i)
+	x := int(f*m + 0.5)
+	return float64(x) / m
+}
+
+func FormatFloat(f float64, decimal int) string {
+	return strconv.FormatFloat(f, 'g', decimal+1, 64)
 }
