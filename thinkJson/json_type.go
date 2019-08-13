95ad22b54845d67936dbf9db63ce47f6171ddd57
禾沛 <13730645556@163.com>
2019-08-13 10:09:57 +0800

    json getList

---
+++ thinkJson/json_type.go
@@ -0,0 +1,15 @@
+package thinkJson
+
+//要将json数据解码写入一个接口类型值，函数会将数据解码为如下类型写入接口：
+//Bool                   对应JSON布尔类型
+//float64                对应JSON数字类型
+//string                 对应JSON字符串类型
+//[]interface{}          对应JSON数组
+//map[string]interface{} 对应JSON对象
+//nil                    对应JSON的null
+type JsonBool bool
+type JsonFloat float64
+type JsonString string
+type JsonList []interface{}
+type JsonMap map[string]interface{}
+type JsonInterface interface {}
--- wav/add_wav_header_test.go
+++ wav/add_wav_header_test.go
@@ -1,13 +1,13 @@
-package file
+package wav
 
 import (
-	"testing"
-	"os"
-	"util/think"
 	"fmt"
+	"os"
+	"testing"
+	"util/think"
 )
 
-func TestAddHeader(t *testing.T){
+func TestAddHeader(t *testing.T) {
 	p, err := os.Open("./test.pcm")
 	think.IsNil(err)
 	defer p.Close()
@@ -20,14 +20,14 @@ func TestAddHeader(t *testing.T){
 
 	fmt.Println(l, info.Size())
 
-
 	w, err := os.Create("./test.wav")
 	think.IsNil(err)
 	defer w.Close()
 
-	w.Write(Header(1,8000, 16, int(info.Size())))
+	w.Write(Header(1, 8000, 16, int(info.Size())))
 	w.Write(buf)
 }
 
-
-
+func TestMath(t *testing.T) {
+	fmt.Println(0x8000)
+}
