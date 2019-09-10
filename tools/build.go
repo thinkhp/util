b1aee2269bc469f295306a53e3cf4209524145e0
禾沛 <13730645556@163.com>
2019-09-10 09:14:46 +0800

    打印cmd增加/n

--- tools/build.go
+++ tools/build.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"runtime"
 )
 
@@ -17,7 +18,7 @@ type Project struct {
 
 var ErrGOOS = errors.New("不支持的操作系统")
 
-func (p Project) Build(aimGOOS string) (output []byte, errput []byte, err error) {
+func (p Project) Build(aimGOOS string) (output []byte, err error) {
 	exeName := p.Name
 	mainFile := p.Path + p.MainFileName
 	binDir := p.Path
@@ -32,53 +33,59 @@ func (p Project) Build(aimGOOS string) (output []byte, errput []byte, err error)
 	case "darwin":
 		suffix = ".sh"
 		cmds = append(cmds, "#!/usr/bin/env bash\n")
-		cmds = append(cmds, "cd "+binDir)
+		cmds = append(cmds, "cd "+binDir+"\n")
 		cmds = append(cmds, fmt.Sprintf("GOOS=%s go build -o %s %s\n", aimGOOS, exeName, mainFile))
 		cmds = append(cmds, "exit 0")
 	case "windows":
 		suffix = ".bat"
+		cmds = append(cmds, binDir[:2]+"\n") //盘符
+		cmds = append(cmds, "cd "+binDir+"\n")
 		cmds = append(cmds, "set GOOS="+aimGOOS+"\n")
-		cmds = append(cmds, binDir[:2]) //盘符
-		cmds = append(cmds, "cd "+binDir)
 		cmds = append(cmds, fmt.Sprintf("go build -o %s %s", exeName, mainFile)+"\n")
 		cmds = append(cmds, "set GOOS=windows\n")
 	case "linux":
 		suffix = ".sh"
 		cmds = append(cmds, "#!/bin/bash\n")
-		cmds = append(cmds, "cd "+binDir)
+		cmds = append(cmds, "cd "+binDir+"\n")
 		cmds = append(cmds, fmt.Sprintf("GOOS=%s go build -o %s %s", aimGOOS, exeName, mainFile)+"\n")
 		cmds = append(cmds, "exit 0")
 	default:
-		return nil, nil, ErrGOOS
+		return nil, ErrGOOS
 	}
 
 	file, err := os.OpenFile("../build"+suffix, os.O_CREATE|os.O_TRUNC|os.O_RDWR, 0770)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 	defer file.Close()
+	fileName, _ := filepath.Abs(file.Name())
+	fmt.Println("脚本文件路径:",fileName)
+
+
+	fmt.Println("脚本内容:")
 	for _, cmd := range cmds {
 		_, err := file.WriteString(cmd)
 		if err != nil {
-			return nil, nil, err
+			return nil, err
 		}
-		fmt.Println(cmd)
+		fmt.Print(cmd)
 	}
 	file.Close()
 
-	//fmt.Println(file.Name())
+
 	// 执行脚本
-	cmd := exec.Command(file.Name())
+	cmd := exec.Command(fileName)
+	//fmt.Println("cmd:",cmd.Args)
+	errput := make([]byte, 0)
 	cmd.Stdout = bytes.NewBuffer(output)
 	cmd.Stderr = bytes.NewBuffer(errput)
 	if err := cmd.Run(); err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
-	//fmt.Println(cmd.Args)
-
+	if len(errput) != 0 {
+		return output, errors.New(string(errput))
+	}
 	//fmt.Println(string(output))
-	//fmt.Println(string(errput))
-
-	return output, errput, nil
+	return output, nil
 }
